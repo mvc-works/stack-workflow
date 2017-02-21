78c37499e45c76df514a736d3b2e23b9b9d825b8
jiyinyiyong <jiyinyiyong@gmail.com>
2017-02-22 01:16:58 +0800

    remove optimizations

--- tasks/build.cljs
+++ tasks/build.cljs
@@ -4,11 +4,4 @@
 (b/build "src"
   {:main 'client.main
    :output-to "main.js"
-   :optimizations :advanced
-   :compiler-options {:language-in :ecmascript5
-                      :pseudo-names true
-                      :static-fns true
-                      :parallel-build true
-                      :optimize-constants true
-                      :source-map true}
-   :target :nodejs})
+   :optimizations :none})

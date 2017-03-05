115e77b8fa3bfe5baf03fc8dc8ac46215a31fa8b
jiyinyiyong <jiyinyiyong@gmail.com>
2017-03-05 15:32:43 +0800

    rename -ref to be a prefix

--- src/client/main.cljs
+++ src/client/main.cljs
@@ -8,13 +8,13 @@
 
 (defn dispatch! [op op-data] )
 
-(defonce store-ref (atom {}))
+(defonce ref-store (atom {}))
 
-(defonce states-ref (atom {}))
+(defonce ref-states (atom {}))
 
 (defn render-app! []
   (let [target (.querySelector js/document "#app")]
-    (render! (comp-container @store-ref) target dispatch! states-ref)))
+    (render! (comp-container @ref-store) target dispatch! ref-states)))
 
 (def ssr-stages
   (let [ssr-element (.querySelector js/document "#ssr-stages")
@@ -27,12 +27,12 @@
     (let [target (.querySelector js/document "#app")]
       (falsify-stage!
        target
-       (render-element (comp-container @store-ref ssr-stages) states-ref)
+       (render-element (comp-container @ref-store ssr-stages) ref-states)
        dispatch!)))
   (render-app!)
-  (add-watch store-ref :gc (fn [] (gc-states! states-ref)))
-  (add-watch store-ref :changes render-app!)
-  (add-watch states-ref :changes render-app!)
+  (add-watch ref-store :gc (fn [] (gc-states! ref-states)))
+  (add-watch ref-store :changes render-app!)
+  (add-watch ref-states :changes render-app!)
   (println "App started!"))
 
 (defn on-jsload! [] (clear-cache!) (render-app!) (println "Code updated."))

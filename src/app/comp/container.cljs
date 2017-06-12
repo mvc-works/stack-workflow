9f6cf3d25d5c6a8d880f9b1dff7ecac3faa4c685
ChenYong <yong.chen04@ele.me>
2017-06-12 17:35:50 +0800

    update respo@0.4.4 ; simplify component

--- src/app/comp/container.cljs
+++ src/app/comp/container.cljs
@@ -1,17 +1,16 @@
 
 (ns app.comp.container
+  (:require-macros (respo.macros :refer (defcomp)))
   (:require [hsl.core :refer [hsl]]
             [respo-ui.style :as ui]
             [respo.alias :refer [create-comp div span]]
             [respo.comp.space :refer [comp-space]]
             [respo.comp.text :refer [comp-text]]))
 
-(def comp-container
-  (create-comp
-   :container
-   (fn [store]
-     (fn [cursor]
-       (div
-        {:style (merge ui/global)}
-        (comp-space "8px" nil)
-        (div {:style ui/button} (comp-text "Demo" nil)))))))
+(defcomp
+ comp-container
+ (store)
+ (div
+  {:style (merge ui/global)}
+  (comp-space "8px" nil)
+  (div {:style ui/button} (comp-text "Demo" nil))))

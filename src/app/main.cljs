47645a7ac6f6356f3503c3dec15baaa0be9c602d
jiyinyiyong <jiyinyiyong@gmail.com>
2017-06-18 13:55:43 +0800

    prefer *store to ref-store

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -5,14 +5,14 @@
             [cljs.reader :refer [read-string]]
             [app.updater.core :refer [updater]]))
 
-(defonce ref-store (atom {:states {}}))
+(defonce *store (atom {:states {}}))
 
 (defn dispatch! [op op-data]
-  (let [next-store (updater @ref-store op op-data)] (reset! ref-store next-store)))
+  (let [next-store (updater @*store op op-data)] (reset! *store next-store)))
 
 (def mount-target (.querySelector js/document ".app"))
 
-(defn render-app! [] (render! (comp-container @ref-store) mount-target dispatch!))
+(defn render-app! [] (render! (comp-container @*store) mount-target dispatch!))
 
 (defn reload! [] (clear-cache!) (render-app!) (println "Code updated."))
 
@@ -20,9 +20,9 @@
 
 (defn main! []
   (if server-rendered?
-    (falsify-stage! mount-target (render-element (comp-container @ref-store)) dispatch!))
+    (falsify-stage! mount-target (render-element (comp-container @*store)) dispatch!))
   (render-app!)
-  (add-watch ref-store :changes render-app!)
+  (add-watch *store :changes render-app!)
   (println "App started."))
 
 (set! (.-onload js/window) main!)
